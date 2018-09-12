8286676aa3122a720dccea52c71031477edf288b
Игорь Шелгинский <engwar@macbook-pro-igor.local>
2018-09-12 13:29:10 +0300

    Hide status bar and try to catch a flash device. If phone has no flash when turn background from black to white and conversely

--- Fonarik/ViewController.swift
+++ Fonarik/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var lightOn = true
 
+    @IBAction func buttonPress(_ sender: UIButton) {
+        lightOn = !lightOn
+        updateFlash()
+    }
+    
+    func updateFlash () {
+        let flashDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        if let flash = flashDevice, flash.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try flash.lockForConfiguration()
+                flash.torchMode = lightOn ? .on : .off
+                flash.unlockForConfiguration()
+            } catch {
+              print(error)
+            }
+        } else {
+            view.backgroundColor = lightOn ? .white : .black
+        }
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateFlash()
     }
 
 
